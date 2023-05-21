4d053bc6b6e4d065ac852bf379c4e6b9cfeaf29a
jasonliu870213 <51989369+jasonliu870213@users.noreply.github.com>
2023-05-21 17:14:01 +0800

    Update exec_test.sh

--- exec_test.sh
+++ exec_test.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 source source /Users/liuchenghsuan/PycharmProjects/jenkins_assignment/venv/bin/activate
+pip install -r requirements.txt
 echo 'gggggggggggggg'
 python -m pytest --alluredir=./allure-results
 
