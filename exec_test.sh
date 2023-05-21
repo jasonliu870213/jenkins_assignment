2f9439d25380a29d981ff5c5cb1b98e1d7d2c1a2
jasonliu870213 <51989369+jasonliu870213@users.noreply.github.com>
2023-05-21 16:54:00 +0800

    Update exec_test.sh

--- exec_test.sh
+++ exec_test.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 source /Users/liuchenghsuan/PycharmProjects/Automation-Test-Program-Batch2-Project/venv/bin/activate
+pip install -r requirements.txt
 ls -l
 echo 'gggggggggggggg'
 python -m pytest --alluredir=./
